7379ea2fe706e077183ac39da2bdf2341c8da039
Dino Pacariz <dinopacariz@gmail.com>
2021-10-11 17:22:22 +0200

    boiler plate, folder structure

---
+++ server/App.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+
+app.get('/api/customers', cors(), (req, res) => {
+  const customers = [
+    {id: 1, firstName: 'John', lastName: 'Doe'},
+    {id: 2, firstName: 'Brad', lastName: 'Traversy'},
+    {id: 3, firstName: 'Mary', lastName: 'Swanson'},
+  ];
+
+  res.json(customers);
+});
+
+app.listen(port, () => `Server running on port ${process.env.PORT || 5000}`);
