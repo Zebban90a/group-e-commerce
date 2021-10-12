bd5dd58b88f996ff6d4d4e89c3b67d927e4f6e89
Kweku Moses <kweku.moses@hotmail.com>
2021-10-12 11:23:28 +0200

    add cors

--- server/App.js
+++ server/App.js
@@ -3,8 +3,9 @@ const cors = require("cors");
 
 const app = express();
 app.use(express.json());
+app.use(cors({ origin: process.env.CLIENT }));
 
-app.get("/api/customers", cors(), (req, res) => {
+app.get("/api/customers", (req, res) => {
   const customers = [
     { id: 1, firstName: "John", lastName: "Doe" },
     { id: 2, firstName: "Brad", lastName: "Traversy" },
