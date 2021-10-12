640229942ad2a45cb41e09fceac0417b7de3ab10
Harald <Harald.Norlin@yh.nackademin.se>
2021-10-12 15:47:51 +0200

    Add routes and controllers for products

--- server/routes/ProductRoute.js
+++ server/routes/ProductRoute.js
@@ -1,7 +1,20 @@
 const express = require("express");
+const {
+  createProduct,
+  updateProduct,
+  deleteProduct,
+  getProducts,
+  getSingleProduct,
+} = require("../controllers/ProductController");
 
 const router = express.Router();
 
-router.route("/").get().post();
+router.route("/").get(getProducts).post(createProduct);
+
+router
+  .route("/:id")
+  .get(getSingleProduct)
+  .patch(updateProduct)
+  .delete(deleteProduct);
 
 module.exports = router;
