ef6dc046eee7114429286551d96e670a83bdc165
Antonio A R <antonio.axelsson@gmail.com>
2021-10-13 11:49:20 +0200

    remove console.log

--- server/App.js
+++ server/App.js
@@ -1,23 +1,15 @@
 const express = require("express");
-const cors = require("cors");
-
 const app = express();
+const cors = require("cors");
 const UserRoute = require("./routes/UserRoute");
 const OrderRoute = require("./routes/OrderRoute");
 const ProductRoute = require("./routes/ProductRoute");
+
 app.use(express.json());
 app.use(cors({ origin: process.env.CLIENT }));
+
 app.use("/api/users", UserRoute);
 app.use("/api/orders", OrderRoute);
 app.use("/api/products", ProductRoute);
 
-app.get("/api/customers", (req, res) => {
-  const customers = [
-    { id: 1, firstName: "John", lastName: "Doe" },
-    { id: 2, firstName: "Brad", lastName: "Traversy" },
-    { id: 3, firstName: "Mary", lastName: "Swanson" },
-  ];
-  res.json(customers);
-});
-console.log(new Date())
 module.exports = app;
