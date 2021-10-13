d9272cfde87a79c17bd3d5ab1d06c6b70a858d12
Antonio A R <antonio.axelsson@gmail.com>
2021-10-13 12:03:46 +0200

    install styled-components, axios, react-router-dom, initialize App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,9 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import Customers from './components/customers';
+import React from 'react';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">React Express Starter</h1>
-        </header>
-        <Customers />
-      </div>
-    );
-  }
+export default function App() {
+  return (
+    <div>
+      <h1>hej</h1>
+    </div>
+  );
 }
-
-export default App;
