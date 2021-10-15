fb5e1fdff3ed2eff8d1e7fd5c02b8992a989519d
Harald <Harald.Norlin@yh.nackademin.se>
2021-10-15 15:28:54 +0200

    navbar component

--- client/src/App.js
+++ client/src/App.js
@@ -1,36 +1,41 @@
-import React from 'react';
-import { Route, Switch } from 'react-router-dom';
-import HomePage from './pages/HomePage';
-import CheckoutPage from './pages/CheckoutPage';
-import UserPage from './pages/UserPage';
-import LoginPage from './pages/LoginPage';
-import RegisterPage from './pages/RegisterPage';
-import ProductListPage from './pages/ProductListPage';
-import ProductDetailPage from './pages/ProductDetailPage';
-import AdminOrdersPage from './pages/AdminOrdersPage';
-import AdminProductsPage from './pages/AdminProductsPage';
-import AdminPage from './pages/AdminPage';
-import AdminProductEditPage from './pages/AdminProductEditPage';
+import React from "react";
+import { Route, Switch } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import CheckoutPage from "./pages/CheckoutPage";
+import UserPage from "./pages/UserPage";
+import LoginPage from "./pages/LoginPage";
+import RegisterPage from "./pages/RegisterPage";
+import ProductListPage from "./pages/ProductListPage";
+import ProductDetailPage from "./pages/ProductDetailPage";
+import AdminOrdersPage from "./pages/AdminOrdersPage";
+import AdminProductsPage from "./pages/AdminProductsPage";
+import AdminPage from "./pages/AdminPage";
+import AdminProductEditPage from "./pages/AdminProductEditPage";
+import NavBar from "./components/NavBar";
 
 export default function App() {
   return (
     <div>
+      <NavBar />
+      {/* <a href="http://localhost:5000/logout">
+        <button>Logout mathafacka</button>
+      </a> */}
       <Switch>
-        <Route path='/products/:id' component={ProductDetailPage} />
-        <Route path='/products' component={ProductListPage} />
+        <Route path="/products/:id" component={ProductDetailPage} />
+        <Route path="/products" component={ProductListPage} />
 
-        <Route path='/checkout' component={CheckoutPage} />
-        <Route path='/register' component={RegisterPage} />
-        <Route path='/login' component={LoginPage} />
-        <Route path='/user' component={UserPage} />
-  
+        <Route path="/checkout" component={CheckoutPage} />
+        <Route path="/register" component={RegisterPage} />
+        <Route path="/login" component={LoginPage} />
+        <Route path="/user" component={UserPage} />
+
         {/* todo general 404 page for nonadmins (and non logged in) */}
-        <Route path='/admin/orders' component={AdminOrdersPage} />
-        <Route path='/admin/products' component={AdminProductsPage} />
-        <Route path='/admin/products/:id' component={AdminProductEditPage} />
-        <Route path='/admin' component={AdminPage} />
+        <Route path="/admin/orders" component={AdminOrdersPage} />
+        <Route path="/admin/products" component={AdminProductsPage} />
+        <Route path="/admin/products/:id" component={AdminProductEditPage} />
+        <Route path="/admin" component={AdminPage} />
 
-        <Route path='/' component={HomePage} />
+        <Route path="/" component={HomePage} />
       </Switch>
     </div>
   );
--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -1,9 +1,59 @@
 import React from 'react';
+import { Link } from "react-router-dom";
+import styled from 'styled-components';
 
 export default function NavBar() {
+  const linkArray = [{
+    "path" : "/products",
+    "name" : "Products"},{
+    "path" : "/admin",
+    "name": "Admin"
+    },
+    {
+      "path" : "/",
+      "name" : "Home"},{
+      "path" : "/user",
+      "name": "User"
+      },
+      {
+        "path" : "/register",
+        "name" : "Register"},{
+        "path" : "/checkout",
+        "name": "Checkout"
+        },
+        {
+          "path" : "/login",
+          "name" : "Login"},{
+          "path" : "/logout",
+          "name": "Logout"
+          }
+  ]
+
+  const StyledNav = styled.nav`
+  background: pink;
+  `
+  const StyledButton = styled.button`
+  background: red;
+  padding: 10px;
+  margin-right: 15px;
+  &:hover{
+   background: blue; 
+  }
+  `
   return (
-    <div>
-      <h1>Hej</h1>
-    </div>
+    <StyledNav>
+      {linkArray.map(link =>{
+        return (
+          <Link to={link.path}>
+              <StyledButton>
+                <span type="button" className="nav-link ml-2">
+                  {link.name}
+                </span>
+              </StyledButton>
+              </Link>
+        )}
+      )}
+      
+    </StyledNav>
   );
 }
