ee57638a1e7a0e3e35307e0b8571aa5b9d1a62cf
Dino Pacariz <dinopacariz@gmail.com>
2021-10-17 00:20:34 +0200

    vertical alignment fixed

--- client/src/components/ProductCard.jsx
+++ client/src/components/ProductCard.jsx
@@ -10,6 +10,9 @@ const Card = styled.div`
   padding: 15px;
   border: 1px solid black;
   height: 400px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
 
   a {
     text-decoration: none;
@@ -20,12 +23,14 @@ const Card = styled.div`
     }
   }
   .details-link{
-    
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
   }
-
   .display-img {
     object-fit: cover;
-    height: 65%;
+    height: 240px;
     width: 100%;
   }
   span {
@@ -37,7 +42,7 @@ const Card = styled.div`
     overflow: hidden;
     text-overflow: ellipsis;
     display: -webkit-box;
-    -webkit-line-clamp: 2; /* number of lines to show */
+    -webkit-line-clamp: 2;
     -webkit-box-orient: vertical;
   }
   .price {
@@ -53,13 +58,12 @@ const Card = styled.div`
     }
   }
   .addToCart {
+    display: flex;
+    justify-self: flex-end;
     background-color: #01da01;
     height: 40px;
   }
 `
-
-//TODO make the title underline appear when hovering over card like on netonnet
-//TODO make price/addToCart/inStock stay on the same height
 
 export default function ProductCard(props) {
   const { _id, title, price, images, quantity} = props.product;
@@ -67,21 +71,27 @@ export default function ProductCard(props) {
   return (
     <Card>
       <Link to={'./products/'+_id} className="details-link">
-        <img
-          className="display-img"
-          src={images[0] || noImage}
-          onError={(e)=>{e.target.src=noImage}}
-          alt="no-img-available"/>
-        <span className="title">{title}</span>
-        <span className="price">{price}</span>
+        <div>
+          <img
+            className="display-img"
+            src={images[0] || noImage}
+            onError={(e)=>{e.target.src=noImage}}
+            alt="no-img-available"/>
+          <span className="title">{title}</span>
+        </div>
+        <div>
+          <span className="price">${price}</span>
+        </div>
       </Link>
-      <div className="availability">
-        <img className="checkmark" src={quantity? checkmark : crossmark}/>
-        <div>{quantity} available</div>
+      <div>
+        <div className="availability">
+          <img className="checkmark" src={quantity? checkmark : crossmark}/>
+          <div>{quantity} available</div>
+        </div>
+        <Link to={'./products/1'}> {/* //TODO make it add to cart */}
+          <button className="addToCart">Add to cart</button>
+        </Link>
       </div>
-      <Link to={'./products/1'}>
-        <button className="addToCart">Add to cart</button>
-      </Link>
     </Card>
   )
 }
