761aff1c9464ac3fb7223a4a0d26bf4a27d2d49f
aaxelr <antonio.axelsson@gmail.com>
2021-10-19 12:19:25 +0200

    rename file, update isAdmin function

--- server/middleware/authentication.js
+++ server/middleware/authentication.js
@@ -2,14 +2,16 @@ exports.isLoggedIn = (req, res, next) => {
   console.log('isloggedin');
   if (req.user) {
     next();
-  } else { res.redirect('http://localhost:3000/login'); }
+  } else {
+    res.redirect('http://localhost:3000/login');
+  }
 };
 
 exports.isAdmin = (req, res, next) => {
   console.log('isadmin');
   console.log(req.user);
   const user = req.user[0];
-  if (user && user.roles && user.roles.isAdmin) { // TODO make so that all users get a roles object
+  if (user && user.roles && user.roles.includes('admin')) {
     next();
   } else {
     res.status(404).end();
