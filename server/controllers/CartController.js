9494e40901693fee9ba5575fced0e770b78bbcd9
Harald <Harald.Norlin@yh.nackademin.se>
2021-10-21 11:47:07 +0200

    finally working cart array

--- server/controllers/CartController.js
+++ server/controllers/CartController.js
@@ -1,27 +1,32 @@
 const User = require('../models/UserModel');
 
+
+
 exports.addToCart = async (req, res) => {
-  const {userId} = req.body.userId;
-  const {productId} = req.body.productId;
+  const userId = req.body.userId;
+  const productId = req.body.productId;
   console.log(`User:${userId}`);
   console.log(`Product:${productId}`);
 
-//   try {
-//     const user = await User.findByIdAndUpdate(userId, productId, {
-//       new: true,
-//       runValidators: true,
-//     });
-//     const updateCart = user.cart.push(productId);
-//     res.status(200).json({
-//       status: 'success',
+  try {
+    const user = await User.findByIdAndUpdate(userId, productId, {
+      new: true,
+      runValidators: true,
+    });
+    const userCart = user.cart;
+    userCart.push(productId);
+    
+    console.log(user);
+    res.status(200).json({
+      status: 'success',
 
-//       updateCart,
+      userCart,
 
-//     });
-//   } catch (err) {
-//     res.status(404).json({
-//       status: 'fail',
-//       message: err,
-//     });
-//   }
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: 'fail',
+      message: err,
+    });
+  }
 };
