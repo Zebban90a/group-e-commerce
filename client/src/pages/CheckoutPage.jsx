948e939d828caf865e598b233e5b9dc4644a4c03
Kweku Moses <kweku.moses@hotmail.com>
2021-10-21 16:50:22 +0200

    render cart on checkout page

--- client/src/pages/CheckoutPage.jsx
+++ client/src/pages/CheckoutPage.jsx
@@ -3,13 +3,9 @@ import axios from 'axios';
 
 export default function CheckoutPage() {
   const mockUserId = '616fdfbc1576abbb9e174e03';
-  const [cart, setCart] = useState({});
+  const [cart, setCart] = useState([]);
 
-  
-  
   // async function getProducts() {
-    
-
 
   //   // console.log(payload);
   //   // const data = await axios({
@@ -21,14 +17,26 @@ export default function CheckoutPage() {
 
   useEffect(() => {
     fetch(`http://localhost:5000/api/checkout/${mockUserId}`)
-    .then((response) => response.json())
-    .then((user) => setCart(user));
-    console.log(cart);
+      .then((response) => response.json())
+      .then((user) => setCart(user.data.user.cart));
   }, []);
+  console.log(cart);
+
   return (
     <div>
-      <h1>Checkout page</h1>
-
+      <h1>CART</h1>
+      {cart.map((product) => (
+        <div>
+          <span>
+            <span>{`Produkt: ${product.Title || 'no name'}`}</span>
+            <br />
+            <span>{`Pris: ${product.Price} kr`}</span>
+          </span>
+        </div>
+      ))}
+      <br />
+      <button>Place Order</button>
     </div>
+
   );
 }
