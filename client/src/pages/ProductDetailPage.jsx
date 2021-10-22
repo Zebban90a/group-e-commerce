a749e12f9a0de24c0eea96cac2cd968374caaef2
Antonio A R <antonio.axelsson@gmail.com>
2021-10-22 17:07:48 +0200

    WIP ProductDetailPage

--- client/src/pages/ProductDetailPage.jsx
+++ client/src/pages/ProductDetailPage.jsx
@@ -1,6 +1,61 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import noImage from '../no-img.png';
+import checkmark from '../checkmark.svg';
+import crossmark from '../crossmark.svg';
+import styled from 'styled-components';
+
+const Container = styled.div`
+  padding: 1.5rem;
+  display: flex;
+  justify-content: center;
+  align-items: flex-start;
+`;
+
+const Card = styled.div`
+  padding: 1rem;
+  display: flex;
+  flex-direction: column;
+  border: solid 1px #000;
+  width: 60vw;
+  min-width: 800px; // TODO fix media queries
+`;
+
+const ImageContainer = styled.div`
+  flex-basis: 50%;
+  padding: 1rem;
+`;
+
+const Image = styled.img`
+  object-fit: cover;
+  height: 400px;
+  width: 100%;
+`;
+
+const InfoContainer = styled.div`
+  flex-basis: 50%;
+  padding: 1rem;
+  .price {
+    font-weight: bold;
+    font-size: 1.5rem;
+  }
+  .availability {
+    display: flex;
+    align-items: center;
+    img {
+      width: 28px;
+      padding-bottom: 3px;
+      margin-right: 5px;
+    }
+  }
+`;
+
+const Button = styled.button`
+  display: flex;
+  justify-self: flex-end;
+  background-color: #01da01;
+  height: 40px;
+`;
 
 export default function ProductDetailPage(props) {
   const [product, setProduct] = useState('');
@@ -11,27 +66,48 @@ export default function ProductDetailPage(props) {
     const path = `/api/products/${id}`;
     const { data } = await axios.get(path);
     setProduct(data.data.product);
-
-  // setProducts(data.data.products);
   }
 
   useEffect(() => {
     getProduct();
   }, []);
+
   return (
-    <div>
-      <div>
-        {!product && <p>Loading..</p>}
-        {product &&
-        <div> 
-        <p>{product.title}</p>
-        <p>{product.description}</p>
-        <p>price: {product.price}</p>
-        <img alt={product.title} src={product.images[0] || noImage} />
-        </div>
-        }
-      </div>
-      <h1>Product detailsss</h1>
-    </div>
+    <Container pad>
+      {!product && <p>Loading..</p>}
+      {product &&
+        <Card>
+          <div>
+            <h1>{product.title}</h1>
+            <p>{product.category}</p>
+          </div>
+          <Container>
+            <ImageContainer>
+              <Image
+                alt={product.title}
+                src={`../${product.images[0]}` || noImage}
+                onError={(e) => { e.target.src = noImage; }}
+              />
+            </ImageContainer>
+            <InfoContainer>
+              <p>{product.description} Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet culpa modi nemo dolorem voluptatem? Cupiditate debitis cumque, quo error officiis recusandae sequi dicta natus fugiat, iusto iure corrupti obcaecati esse soluta! Quibusdam repudiandae ea veritatis earum cumque nemo maxime praesentium est hic ab, eligendi aliquid porro quo labore, exercitationem qui?</p>
+              <p className="price">&#36;{product.price}</p>
+              <div className="availability">
+                {!product.quantity &&
+                  <img src={crossmark} alt="unavailable" />
+                }
+                {product.quantity &&
+                  <img src={checkmark} alt="available" />
+                }
+                <p>{product.quantity} available</p>
+              </div>
+              <p>Manufacturer: {product.manufacturer}</p>
+              <p>Weight: {product.weight}g</p>
+              <Button>TODO: Add add to cart function</Button>
+            </InfoContainer>
+          </Container>
+        </Card>
+      }
+    </Container>
   );
 }
