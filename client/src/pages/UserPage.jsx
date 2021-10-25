b957ffed2498abaf8d0d47e6b3b124531e057ba3
Antonio A R <antonio.axelsson@gmail.com>
2021-10-25 17:07:33 +0200

    WIP userpage render orders

--- client/src/pages/UserPage.jsx
+++ client/src/pages/UserPage.jsx
@@ -3,7 +3,68 @@ import axios from 'axios';
 
 export default function UserPage() {
   const [userData, setUserData] = useState({});
-  const [orders, setOrders] = useState(null);
+  const [orders, setOrders] = useState(testOrders);
+
+  const testOrders = [{
+    "_id": "616593c4e9eb5c2a271c6b38",
+    "purchaser": "616584cf8b04b9069807c7ae",
+    "products": [
+      {
+        "productId": "61658a1c2e620ab1b18dd34a",
+        "quantity": 1,
+        "productPrice": 400,
+      },
+      {
+        "productId": "61658a1c2e620ab1b18dd123",
+        "quantity": 12,
+        "productPrice": 4300,
+      }
+    ],
+    "orderTotal": 400,
+    "freight": 20,
+    "status": 0,
+    "address": {
+      "zip": 12345,
+      "city": "Gotham",
+      "street": "Waynestreet",
+      "houseNumber": 10
+    },
+    "contact": {
+      "tel": 555555123,
+      "email": "kweku@gmail.com",
+      "_id": "616593c4e9eb5c2a271c6b3a"
+    }
+  },
+  {
+    "_id": "616593c4e9eb5c2a271c6123",
+    "purchaser": "616584cf8b04b9069807c7ae",
+    "products": [
+      {
+        "productId": "61658a1c2e620ab1b18dd34a",
+        "quantity": 1,
+        "productPrice": 400,
+      },
+      {
+        "productId": "61658a1c2e620ab1b18dd123",
+        "quantity": 12,
+        "productPrice": 4300,
+      }
+    ],
+    "orderTotal": 400,
+    "freight": 20,
+    "status": 0,
+    "address": {
+      "zip": 12345,
+      "city": "Gotham",
+      "street": "Waynestreet",
+      "houseNumber": 10
+    },
+    "contact": {
+      "tel": 555555123,
+      "email": "kweku@gmail.com",
+      "_id": "616593c4e9eb5c2a271c6b3a"
+    }
+  },];
 
   async function getUser() {
     const path = '/api/users';
@@ -18,47 +79,69 @@ export default function UserPage() {
 
   useEffect(() => {
     getUser();
+    console.log(orders);
   }, []);
 
+  // TODO fix styling, make components
   return (
     <div>
       <div>
         <p>{userData.fullName}</p>
         <p>{userData.email}</p>
       </div>
-
       {orders
         && (
-        <div>
-          <table>
-            <thead>
-              <th>
-                Order ID:
-              </th>
-              <th>
-                Order total:
-              </th>
-              <th>
-                Status:
-              </th>
-              <th>
-                Products:
-              </th>
-            </thead>
-            <tbody>
-              {orders.map((order) => {
+          <div style={{ border: '1px solid #000' }}>
+            <table>
+              <thead>
                 <tr>
-                  <td>{order._id}</td>
-                  <td>{order.orderTotal}</td>
-                  <td>{order.status}</td>
-                  <td>{order.products}</td>
-                </tr>;
-              })}
-            </tbody>
-          </table>
-        </div>
-        )}
+                  <th>
+                    Order ID:
+                  </th>
+                  <th>
+                    Order total:
+                  </th>
+                  <th>
+                    Status:
+                  </th>
+                  <th>
+                    Products:
+                  </th>
+                </tr>
+              </thead>
+              <tbody >
+                {orders.map((order) => {
+                  return (
+                    <tr key={order._id} >
+                      <td>{order._id}</td>
+                      <td>{order.orderTotal}</td>
+                      <td>{order.status}</td>
+                      {order.products.map(product => {
+                        return (
+                          <div>
+                            <tr>
+                              <th>product id</th>
+                              <th>product quantity</th>
+                              <th>product price</th>
+                            </tr>
+                            <tr>
+                              <td>{product.productId}</td>
+                              <td>{product.quantity}</td>
+                              <td>{product.productPrice}</td>
+                            </tr>
+                          </div>
+                        )
+                      })
+                      }
+                    </tr>
+                  )
+                })}
+              </tbody>
+            </table >
+          </div >
+        )
+      }
 
-    </div>
+    </div >
   );
 }
