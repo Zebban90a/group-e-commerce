72adf6e417ff72cb10fda4ae4306bad8d5268e93
Sebastian Orbom <zebban90a@gmail.com>
2021-10-26 13:51:05 +0200

    WIP admin orderpage

--- client/src/pages/AdminPage.jsx
+++ client/src/pages/AdminPage.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 export default function AdminPage() {
   return (
     <div>
       <h1>admin overview</h1>
+      <Link to="/admin/products"> Products </Link>
+      <br />
+      <Link to="/admin/orders"> Order </Link>
     </div>
   );
 }
