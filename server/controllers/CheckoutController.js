aaf6e41482a43b220cf8a1124528f21bab37877b
Sebastian Orbom <zebban90a@gmail.com>
2021-10-27 14:00:14 +0200

    fixed problem NAN and changed freightlogic

--- server/controllers/CheckoutController.js
+++ server/controllers/CheckoutController.js
@@ -1,33 +1,37 @@
 const User = require('../models/UserModel');
 const Order = require('../models/OrderModel');
+
 exports.placeOrder = async (req, res) => {
   const userId = req.user._id;
-  const cart = req.user.cart;
-  let orderTotal = 0;
-  let freightTotal = 0;
-  
-  for(let i = 0; i < cart.length; i++){
-     orderTotal += cart[i].price;
-     freightTotal += cart[i].weight; //TODO figure out a reasonable freight figure per item or weight.
-  }
-  console.log('TOTAL:',orderTotal);
-  const addressData = req.body.formInput;
-  console.log(addressData);
-  console.log(freightTotal) //REVIEW NaN according to error
+  const { cart } = req.user;
+  let productTotal = 0;
+
+  // REVIEW NaN according to error
   // const userData = {
   //   cart: []
   // }
-  const orderData = {
-    purchaser: userId,
-    cart: cart,
-    orderTotal: orderTotal,
-    freight: freightTotal,
-    status: 0,
-    shippingAddress: addressData,
-  }
+  const setOrderData = async () => {
+    for (let i = 0; i < cart.length; i++) {
+      productTotal += cart[i].price;
+      // freightTotal += cart[i].weight; // TODO figure out a reasonable freight figure per item or weight.
+    }
+    const freight = productTotal > 100 ? 0 : 50;
+    const orderTotal = freight + productTotal;
+    const addressData = req.body.formInput;
+
+    return {
+      purchaser: userId,
+      cart,
+      orderTotal,
+      freight,
+      status: 0,
+      shippingAddress: addressData,
+    };
+  };
+  const orderData = await setOrderData();
   try {
     const order = await Order.create(orderData);
-    console.log("ORDER SUCCESS");
+    console.log('ORDER SUCCESS');
     res.status(200).json({
       status: 'success',
       data: {
@@ -36,8 +40,8 @@ exports.placeOrder = async (req, res) => {
     });
     res.end();
   } catch (err) {
-    console.log("CHECKOUT ERROR");
-    console.log(err)
+    console.log('CHECKOUT ERROR');
+    console.log(err);
     res.status(404).json({
       status: 'fail',
       message: err,
