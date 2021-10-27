a25c207a8214cc93f35279f666fffeebe224893a
Harald <Harald.Norlin@yh.nackademin.se>
2021-10-27 11:42:31 +0200

    fixed quantity in order

--- server/controllers/CheckoutController.js
+++ server/controllers/CheckoutController.js
@@ -4,34 +4,55 @@ exports.placeOrder = async (req, res) => {
   const userId = req.user._id;
   const cart = req.user.cart;
   let orderTotal = 0;
-  let freightTotal = 0;
+  //let freightTotal = 0;
+  let occurrences = [];
+  let newCart = [];
+
   
   for(let i = 0; i < cart.length; i++){
      orderTotal += cart[i].price;
-     freightTotal += cart[i].weight; //TODO figure out a reasonable freight figure per item or weight.
+     //freightTotal += cart[i].weight; //TODO figure out a reasonable freight figure per item or weight.
+     occurrences.push(cart[i].id);
+     console.log(cart[i].weight);
   }
-  console.log('TOTAL:',orderTotal);
+  //console.log(occurrences);
+  const quantity = occurrences.reduce(function (acc, curr) {
+    return acc[curr] ? ++acc[curr] : acc[curr] = 1, acc
+  }, {});
+
+  for (const [key, value] of Object.entries(quantity)) {
+    newCart.push({id:key, quantity:value});
+  }
+  
+  console.log(newCart);
+  
+  // console.log('TOTAL:',orderTotal);
   const addressData = req.body.formInput;
-  console.log(addressData);
-  console.log(freightTotal) //REVIEW NaN according to error
+  // console.log(addressData);
+  // console.log(freightTotal) //REVIEW NaN according to error
   // const userData = {
   //   cart: []
   // }
+  const emptyCart={
+    cart= []
+  }
   const orderData = {
     purchaser: userId,
-    cart: cart,
+    cart: newCart,
     orderTotal: orderTotal,
-    freight: freightTotal,
+    //freight: freightTotal,
     status: 0,
     shippingAddress: addressData,
   }
   try {
+    const user = await User.findOneAndUpdate()
     const order = await Order.create(orderData);
     console.log("ORDER SUCCESS");
     res.status(200).json({
       status: 'success',
       data: {
         order,
+        user
       },
     });
     res.end();
