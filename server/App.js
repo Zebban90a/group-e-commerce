550d00dd35a4e329fe62700f4a4ecaa576131cbe
Dino Pacariz <dinopacariz@gmail.com>
2021-10-30 14:57:17 +0200

    testing cors setting

--- server/App.js
+++ server/App.js
@@ -26,8 +26,6 @@ app.use(cors({
 }));
 
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', "*");
-  res.header('Access-Control-Allow-Methods', 'GET,POST,DELETE');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested With, Content-Type, Accept');
   res.setHeader(
     "Access-Control-Allow-Headers",
