71f60053c5a423d770979de3ace66ab0471da780
Dino Pacariz <dinopacariz@gmail.com>
2021-10-30 12:40:54 +0200

    testing cors setting

--- server/App.js
+++ server/App.js
@@ -16,12 +16,19 @@ const CheckoutRoute = require('./routes/CheckoutRoute');
 const passport = require('./config/passport');
 const session = require('./config/session');
 
-console.log('change cors 12:25');
 // App Use
 app.use(cors({
   origin: process.env.CLIENT,
-  "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
 }));
+
+console.log('change cors 12:40');
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', process.env.CLIENT);
+  res.header('Access-Control-Allow-Methods', 'GET,POST,DELETE');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested With, Content-Type, Accept');
+  next();
+});
+
 app.use(session);
 app.use(express.json());
 app.use(passport.initialize());
