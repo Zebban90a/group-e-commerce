33e55224d2d56c551964b28422cec1eae7946952
Dino Pacariz <dinopacariz@gmail.com>
2021-10-30 13:22:17 +0200

    testing cors setting

--- server/App.js
+++ server/App.js
@@ -22,7 +22,7 @@ app.use(cors({
   origin: '*',
   // origin: process.env.CLIENT,
   methods: 'GET, POST, OPTIONS, PUT, PATCH, DELETE',
-  credentials: true,
+  //credentials: true,
 }));
 
 app.use(session);
