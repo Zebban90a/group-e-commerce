1c8a38267bae004b18a51e9d34c8df4ca1f6bbf5
Antonio A R <antonio.axelsson@gmail.com>
2021-11-02 20:24:32 +0100

    install datauri, add to multer mw to stringify img buffer

--- server/middleware/multer.js
+++ server/middleware/multer.js
@@ -1,7 +1,18 @@
 const multer = require('multer');
+const DataUri = require('datauri');
+const path = require('path');
 
+// Setup multer
 const storage = multer.memoryStorage();
 
 const multerUploads = multer({ storage }).single('image');
 
-module.exports = multerUploads;
+// DataURI
+const dUri = new DataUri();
+
+// pass the request object and format the buffer and return a string blob.
+const dataUri = (req) => {
+  dUri.format(path.extname(req.file.originalname).toString(), req.file.buffer);
+};
+
+module.exports = { multerUploads, dataUri };
