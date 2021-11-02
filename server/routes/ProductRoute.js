cdf9f60884fdfa3cf684bf5692f79f25f34cbc63
Antonio A R <antonio.axelsson@gmail.com>
2021-11-03 00:06:32 +0100

    add comments, remove old code

--- server/routes/ProductRoute.js
+++ server/routes/ProductRoute.js
@@ -1,33 +1,6 @@
 const express = require('express');
-// const cloudinary = require('cloudinary');
 const { multerUploads, dataUri } = require('../middleware/multer');
 const uploadToCloudinary = require('../config/cloudinary');
-// const multer = require('multer');
-// const path = require('path');
-
-/* const imageStorage = multer.diskStorage({
-  // Destination to store image
-  destination: 'images',
-  filename: (_req, file, cb) => {
-    cb(null,
-      `${file.fieldname}_${Date.now()}${path.extname(file.originalname)}`);
-    // file.fieldname is name of the field (image)
-    // path.extname get the uploaded file extension
-  },
-});
-
-const imageUpload = multer({
-  storage: imageStorage,
-  limits: {
-    fileSize: 1000000,
-  },
-  fileFilter(_req, file, cb) {
-    if (!file.originalname.match(/\.(png|jpg)$/)) {
-      return cb(new Error('Please upload a Image'));
-    }
-    return cb(undefined, true);
-  },
-}).single('image'); */
 
 const {
   createProduct,
@@ -37,19 +10,20 @@ const {
   getSingleProduct,
 } = require('../controllers/ProductController');
 
-const {
-  isAdmin,
-} = require('../middleware/authentication');
+const { isAdmin } = require('../middleware/authentication');
 const Product = require('../models/ProductModel');
 
 const router = express.Router();
+
 router
   .route('/')
   .get(getProducts)
-  .post(isAdmin, /* imageUpload,  */ createProduct);
+  // TODO re-enable multerUploads
+  .post(isAdmin, /* multerUploads,  */ createProduct);
 
 router
   .route('/cloudinarytest')
+  // TODO update createProduct with the contents of async function
   .post(multerUploads, async (req, res) => {
     try {
       const file = dataUri(req).content;
@@ -81,24 +55,12 @@ router
       });
     }
   });
-/* if (req.file) {
-    const file = dataUri(req).content;
-    return cloudinary.uploader.upload(file).then((result) => {
-      const image = result.url;
-      return res.status(200).json({
-        messge: 'Your image has been uploded successfully to cloudinary',
-        data: { image },
-      });
-    }).catch((err) => res.status(400).json({
-      messge: 'someting went wrong while processing your request',
-      data: { err },
-    }));
-  } */
 
 router
-  .route('/TABORT/:id') // FIXME change path to just '/:id' when testing is done.
+  .route('/TABORT/:id') // FIXME change path to just '/:id' when testing with /cloudinary route is done.
   .get(getSingleProduct)
-  .patch(isAdmin, /* imageUpload,  */ updateProduct)
+  // TODO update updateProducts to implement new image functionality
+  .patch(isAdmin, /* multerUploads,  */ updateProduct)
   .delete(isAdmin, deleteProduct);
 
 module.exports = router;
