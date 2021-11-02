53a63caac6f5bae455dd7fdeb95bf0f97d6aad1c
Dino Pacariz <dinopacariz@gmail.com>
2021-11-02 09:05:28 +0100

    changed callback url in passport

--- server/config/passport.js
+++ server/config/passport.js
@@ -21,7 +21,7 @@ passport.deserializeUser(async (user, cb) => {
 passport.use(new GoogleStrategy({
   clientID: process.env.GOOGLE_CLIENT_ID,
   clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  callbackURL: 'https://group-e-commerce-client.herokuapp.com/auth/google/callback',
+  callbackURL: 'https://group-e-commerce.herokuapp.com/auth/google/callback',
   passReqToCallback: true,
 },
 (async (request, accessToken, refreshToken, profile, done) => {
