b42a55abcffa06c4f7e8383b3fba6cebad9bb29c
Antonio A R <antonio.axelsson@gmail.com>
2021-11-02 22:16:57 +0100

    add limits and filefilter

--- server/middleware/multer.js
+++ server/middleware/multer.js
@@ -5,12 +5,24 @@ const path = require('path');
 // Setup multer
 const storage = multer.memoryStorage();
 
-const multerUploads = multer({ storage }).single('image');
+const multerUploads = multer({
+  storage,
+  limits: {
+    fileSize: 1000000,
+  },
+  fileFilter(_req, file, cb) {
+    if (!file.originalname.match(/\.(png|jpg)$/)) {
+      return cb(new Error('Please upload a Image'));
+    }
+    return cb(undefined, true);
+  },
+}).single('image');
 
 // DataURI
 const parser = new DatauriParser();
 
 // pass the request object and format the buffer and return a string blob.
+// eslint-disable-next-line max-len
 const dataUri = (req) => parser.format(path.extname(req.file.originalname).toString(), req.file.buffer);
 
 module.exports = { multerUploads, dataUri };
