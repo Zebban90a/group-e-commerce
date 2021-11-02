0bf64e537eeae920f4824e406f865c60cdf695f6
Antonio A R <antonio.axelsson@gmail.com>
2021-11-02 23:53:20 +0100

    update cloudinary setup

--- server/config/cloudinary.js
+++ server/config/cloudinary.js
@@ -1,14 +1,22 @@
 const cloudinary = require('cloudinary').v2;
 require('dotenv').config();
 
-const cloudinaryConfig = (req, res, next) => {
-  cloudinary.config({
-    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-    api_key: process.env.CLOUDINARY_API_KEY,
-    api_secret: process.env.CLOUDINARY_API_SECRET,
-    secure: true,
-  });
-  next();
-};
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+  secure: true,
+});
 
-module.exports = cloudinaryConfig;
+const uploadToCloudinary = (path, folder) => cloudinary.uploader.upload(path, {
+  folder,
+})
+  .then((data) => ({
+    url: data.url,
+    public_id: data.public_id,
+  }))
+  .catch((error) => {
+    console.error(error);
+  });
+
+module.exports = uploadToCloudinary;
