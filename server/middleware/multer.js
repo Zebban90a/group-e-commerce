cb4edfb78c30afc28a0653d1b33e3f5ac298d732
Antonio A R <antonio.axelsson@gmail.com>
2021-11-03 15:00:39 +0100

    remove filefilter

--- server/middleware/multer.js
+++ server/middleware/multer.js
@@ -10,12 +10,6 @@ const multerUploads = multer({
   limits: {
     fileSize: 1000000,
   },
-  fileFilter(_req, file, cb) {
-    if (!file.originalname.match(/\.(png|jpg)$/)) {
-      return cb(new Error('Please upload an image'));
-    }
-    return cb(undefined, true);
-  },
 }).single('images');
 
 // DataURI
@@ -24,8 +18,8 @@ const parser = new DatauriParser();
 // pass the request object and format the buffer and return a string blob.
 // eslint-disable-next-line max-len
 const dataUri = (req) => {
-  console.log(req.file.originalname);
+  console.log('logging from dataUri: ', req.file.originalname);
   return parser.format(path.extname(req.file.originalname).toString(), req.file.buffer);
-}
+};
 
 module.exports = { multerUploads, dataUri };
