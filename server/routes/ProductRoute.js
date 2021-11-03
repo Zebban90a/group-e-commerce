a814bb88b32a9330e7c57e1b7e2e003f016a22ce
Antonio A R <antonio.axelsson@gmail.com>
2021-11-03 14:39:05 +0100

    remove test route

--- server/routes/ProductRoute.js
+++ server/routes/ProductRoute.js
@@ -1,6 +1,5 @@
 const express = require('express');
-const { multerUploads, dataUri } = require('../middleware/multer');
-const uploadToCloudinary = require('../config/cloudinary');
+const { multerUploads } = require('../middleware/multer');
 
 const {
   createProduct,
@@ -11,7 +10,6 @@ const {
 } = require('../controllers/ProductController');
 
 const { isAdmin } = require('../middleware/authentication');
-const Product = require('../models/ProductModel');
 
 const router = express.Router();
 
@@ -22,42 +20,7 @@ router
   .post(/* isAdmin, */ multerUploads, createProduct);
 
 router
-  .route('/cloudinarytest')
-  // TODO update createProduct with the contents of async function
-  .post(multerUploads, async (req, res) => {
-    try {
-      const file = dataUri(req).content;
-      console.log('req.body', req.body);
-      console.log('req.body.title', req.body.title);
-      const imageData = await uploadToCloudinary(file, 'images');
-      console.log(imageData);
-      const formInputData = req.body; // NOTE might have to use JSON.parse()
-      const productExists = await Product.exists({
-        title: formInputData.title,
-      });
-      if (productExists) {
-        throw Error('Product already exists');
-      } else {
-        const deployedData = formInputData;
-        deployedData.images = imageData.url;
-        const newProduct = await Product.create(deployedData);
-        res.status(201).json({
-          status: 'success',
-          data: {
-            newProduct,
-          },
-        });
-      }
-    } catch (error) {
-      res.status(400).json({
-        status: 'fail',
-        message: error.message,
-      });
-    }
-  });
-
-router
-  .route('/TABORT/:id') // FIXME change path to just '/:id' when testing with /cloudinary route is done.
+  .route('/:id')
   .get(getSingleProduct)
   // TODO update updateProducts to implement new image functionality
   .patch(isAdmin, /* multerUploads,  */ updateProduct)
