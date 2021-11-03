423d4cc33b3d81b9d444fcd689549182935dcb5c
Antonio A R <antonio.axelsson@gmail.com>
2021-11-03 15:20:28 +0100

    remove path import, remove express.static

--- server/App.js
+++ server/App.js
@@ -1,7 +1,6 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
-const path = require('path');
 
 const app = express();
 
@@ -27,12 +26,6 @@ app.use(session);
 app.use(express.json());
 app.use(passport.initialize());
 app.use(passport.session());
-// app.use('/images', express.static(path.join(__dirname, 'images')));
-
-app.use((err, req, res, next) => {
-  console.log('This is the invalid field ->', err.field);
-  next(err);
-});
 
 // Routes
 app.use('/api/users', UserRoute);
